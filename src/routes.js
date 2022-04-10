f1875afa6509a3263b49c120614e9082e3ec2845
Muhammad Daffa' A.R <daffavcd@gmail.com>
2022-04-10 10:08:46 +0700

    able to post

---
+++ src/routes.js
@@ -0,0 +1,54 @@
+const { addBookHandler } = require('./handler/addBookHandler');
+
+const routes = [
+  {
+    method: 'POST',
+    path: '/books',
+    handler: addBookHandler,
+  },
+  {
+    method: 'GET',
+    path: '/',
+    handler: (request, h) => {
+      return 'Homepage';
+    },
+  },
+  {
+    method: '*',
+    path: '/',
+    handler: (request, h) => {
+      return 'Halaman tidak dapat diakses dengan method tersebut';
+    },
+  },
+  {
+    method: 'GET',
+    path: '/users/{username?}',
+    handler: (request, h) => {
+      const { username = 'stranger' } = request.params;
+      return `Hello, ${username}!`;
+    },
+  },
+  {
+    method: 'GET',
+    path: '/about',
+    handler: (request, h) => {
+      return 'About page';
+    },
+  },
+  {
+    method: '*',
+    path: '/about',
+    handler: (request, h) => {
+      return 'Halaman tidak dapat diakses dengan method';
+    },
+  },
+  {
+    method: '*',
+    path: '/{any*}',
+    handler: (request, h) => {
+      return 'Route not found';
+    },
+  },
+];
+
+module.exports = routes;
