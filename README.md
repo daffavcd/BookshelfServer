cfb6fa8a0fc99416853e15e674e1c8268577aaad
Muhammad Daffa' A.R <daffavcd@gmail.com>
2022-04-10 14:04:02 +0700

    add get all book by params(name,isFinished,isRead)

---
+++ README.md
@@ -0,0 +1,7 @@
+# API-Bookshelf
+
+<img src="/images/1.png" title="Screenshot 1"/>
+
+## API-Bookshelf
+
+This Project is one of my project during Front-end & Back-end web batch class.
--- src/handler/getAllBookHandler.js
+++ src/handler/getAllBookHandler.js
@@ -3,7 +3,6 @@ const getAllBookHandler = (request, h) => {
   const { name, reading, finished } = request.query;
 
   let isRead, isFinish;
-
   if (reading != null && reading == 1) {
     isRead = true;
   } else {
@@ -16,18 +15,73 @@ const getAllBookHandler = (request, h) => {
     isFinish = false;
   }
 
-  const response = h.response({
-    status: 'success',
-    data: {
-      books: books.map((book) => ({
-        id: book.id,
-        name: book.name,
-        publisher: book.publisher,
-      })),
-    },
-  });
-  response.code(200);
-  return response;
+  if (name == undefined && reading == undefined && finished == undefined) {
+    const response = h.response({
+      status: 'success',
+      data: {
+        books: books.map((book) => ({
+          id: book.id,
+          name: book.name,
+          publisher: book.publisher,
+        })),
+      },
+    });
+    response.code(200);
+    return response;
+  }
+
+  if (reading != undefined) {
+    const response = h.response({
+      status: 'success',
+      data: {
+        books: books
+          .filter((book) => book.reading == isRead)
+          .map((book) => ({
+            id: book.id,
+            name: book.name,
+            publisher: book.publisher,
+          })),
+      },
+    });
+    response.code(200);
+    return response;
+  }
+
+  if (finished != undefined) {
+    const response = h.response({
+      status: 'success',
+      data: {
+        books: books
+          .filter((book) => book.finished == isFinish)
+          .map((book) => ({
+            id: book.id,
+            name: book.name,
+            publisher: book.publisher,
+          })),
+      },
+    });
+    response.code(200);
+    return response;
+  }
+
+  if (name != undefined) {
+    const response = h.response({
+      status: 'success',
+      data: {
+        books: books
+          .filter((book) =>
+            book.name.toLowerCase().includes(name.toLowerCase())
+          )
+          .map((book) => ({
+            id: book.id,
+            name: book.name,
+            publisher: book.publisher,
+          })),
+      },
+    });
+    response.code(200);
+    return response;
+  }
 };
 
 module.exports = { getAllBookHandler };
